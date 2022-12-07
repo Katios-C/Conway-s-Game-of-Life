c5fab1d11beb32b59f016f9c40776e3835efc12e
Katios <kate.chonkina@gmail.com>
2022-12-07 18:36:41 +0600

    Initial Commit

---
+++ Conway's Game of Life/Conway_s_Game_of_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conway_s_Game_of_LifeApp.swift
+//  Conway's Game of Life
+//
+//  Created by Екатерина Чернова on 07.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Conway_s_Game_of_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
